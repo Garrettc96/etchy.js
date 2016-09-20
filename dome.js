2838f12e14f2f46208b67a678f9522f50e00f1e2
Garrett Coffman <garrettc96@gmail.com>
2016-09-19 22:44:11 -0400

    made first letter!

--- dome.js
+++ dome.js
@@ -6,6 +6,73 @@ window.dome = (function () {
         }
         this.length = els.length;
     }
+    // ========= UTILS =========
+    Dome.prototype.forEach = function (callback) {
+        this.map(callback);
+        return this; 
+    };
+    Dome.prototype.map = function (callback) {
+        var results = [];
+        for (var i = 0; i < this.length; i++) {
+            results.push(callback.call(this, this[i], i));
+        }
+        return results; //.length > 1 ? results : results[0];
+    };
+    Dome.prototype.mapOne = function (callback) {
+        var m = this.map(callback);
+        return m.length > 1 ? m : m[0];
+    };
+
+    // ========== DOM MANIPULATION ==========
+    Dome.prototype.text = function (text) {
+        if (typeof text !== "undefined") {
+            return this.forEach(function (el) {
+                el.innerText = text;
+            });
+        } else {
+            return this.mapOne(function (el) {
+                return el.innerText;
+            });
+        }
+    };
+
+    Dome.prototype.html = function (html) {
+        if (typeof html !== "undefined") {
+            return this.forEach(function (el) {
+                el.innerHTML = html;
+            });
+        } else {
+            return this.mapOne(function (el) {
+                return el.innerHTML;
+            });
+        }
+    };
+
+    Dome.prototype.addClass = function (classes) {
+        var className = "";
+        if (typeof classes !== 'string') {
+            for (var i = 0; i < classes.length; i++) {
+               className += " " + classes[i];
+            }
+        } else {
+            className = " " + classes;
+        }
+        return this.forEach(function (el) {
+            el.className += className;
+        });
+    };
+
+    Dome.prototype.removeClass = function (clazz) {
+        return this.forEach(function (el) {
+            var cs = el.className.split(' '), i;
+
+            while ( (i = cs.indexOf(clazz)) > -1) { 
+                cs = cs.slice(0, i).concat(cs.slice(++i));
+            }
+            el.className = cs.join(' ');
+        });
+    };
+
 
     var dome = {
         get: function (selector) {
--- etchy.js
+++ etchy.js
@@ -8,52 +8,99 @@ window.etchy = (function () {
 		canvas.height = 1000;
 		canvas.width = 1000;
 		var gfx = canvas.getContext("2d")
+		gfx.lineWidth = 1
 		var canvasInfo = {
-			x : 0,
-			y : 0,
-			charWidth: 50
+			xStart : 100.5,
+			yStart : 100.5,
+			x : 100.5,
+			y : 100.5,
+			charWidth: 50,
+			gfx : gfx
 		}
-		gfx.moveTo(0,0)
+		gfx.moveTo(100.5,100.5)
 		//halfCircle(1,gfx)
 		//drawLine(0,0,100,100,gfx)
 		//drawLine(200,50,10,100,gfx)
-		drawCircle(50,50,50,gfx,canvasInfo)
+		//drawCircle(canvasInfo,gfx)
+		drawA(canvasInfo)
 		
+	}
+	drawA = function(canvasInfo){
+		drawCircle(canvasInfo)
+		console.log (canvasInfo.x)
+		console.log(canvasInfo.y)
+		canvasInfo.gfx.moveTo(canvasInfo.xStart + canvasInfo.charWidth,canvasInfo.yStart + canvasInfo.charWidth/2)
+		canvasInfo.x = canvasInfo.xStart + canvasInfo.charWidth
+		canvasInfo.y = canvasInfo.yStart + canvasInfo.charWidth/2
+		quartDown(canvasInfo)
+		console.log (canvasInfo.x)
+		console.log(canvasInfo.y)
 	}
 	Etchy.prototype.sketch = function(input){
 		var charWidth = canvas/input.length;
 		
 	}
 	drawLine = function(canvasInfo,xMove,yMove){
-		gfx.moveTo(canvasInfo.x,canvasInfo.y)
-		gfx.lineTo(canvasInfo.x + xMove,canvasInfo.y+yMove)
-		gfx.stroke()
-		gfx.moveTo(canvasInfo.x+xMove,canvasInfo.y+yMove)
-		canvasInfo.x = canvasInfo.x+xMove
-		canvasInfo.y = canvasInfo.y+yMove
-	}
-	drawCircle = function(canvasInfo,gfx){
-		
-		//gfx.moveTo(startX+width/4,startY)
-		halfStraight(canvasInfo)
-		gfx.lineTo(startX+(3*width)/4,startY)
-		gfx.lineTo(startX+width, startY + width/4)
-		gfx.lineTo(startX + width, startY+(3*width)/4)
-		gfx.stroke()	
+		//canvasInfo.gfx.moveTo(canvasInfo.x,canvasInfo.y)
+		//xMove += 0.5
+		//TODO add scale  into canvasInfo
+		/*for (var x = 0; x < 100 * scale; x++){
+		for (var y = 0; y < 100 * scale; y++){
+			count++;
+			setTimeout(function(){	
+				
+				gfx.lineTo(pos.x,pos.y)
+				gfx.stroke()
+				
+				console.log("x: " + pos.x)
+				pos.x += 1;
 
+			},5*count,pos)
+		}
+	}*/
+		console.log("Before" + canvasInfo.y + "yMove" + yMove)
+		//yMove += 0.5
+		yMove = parseInt(yMove)
+		canvasInfo.gfx.lineTo(canvasInfo.x + xMove+ 0.0,canvasInfo.y+yMove+ 0.0)
+		canvasInfo.gfx.stroke()
+		canvasInfo.gfx.moveTo(canvasInfo.x+xMove+ 0.0,canvasInfo.y+yMove + 0.0)
+		canvasInfo.x +=xMove
+		canvasInfo.y +=yMove	
+		console.log("After: " + canvasInfo.y)
+	}
+	
+	drawCircle = function(canvasInfo){
+		canvasInfo.x += canvasInfo.charWidth/4
+		canvasInfo.gfx.moveTo(canvasInfo.x,canvasInfo.y)
+		halfStraight(canvasInfo)
+		diagLine(canvasInfo,1,1)
+		quartDown(canvasInfo)
+		diagLine(canvasInfo,-1,1)
+		halfStraightBack(canvasInfo)
+		diagLine(canvasInfo,-1,-1)
+		quartUp(canvasInfo)
+		diagLine(canvasInfo,1,-1)
+		//diagLine(canvasInfo,1,-1)
+		//gfx.lineTo(startX+(3*width)/4,startY)
+		//gfx.lineTo(startX+width, startY + width/4)
+		//gfx.lineTo(startX + width, startY+(3*width)/4)
+		//gfx.stroke()	
+	}
+	halfStraightBack = function(canvasInfo){
+		drawLine(canvasInfo,-canvasInfo.charWidth/2,0)
 	}
 	halfStraight = function(canvasInfo){
-		drawLine(canvasInfo,canvasInfo.width/2,0)
+		drawLine(canvasInfo,canvasInfo.charWidth/2,0)
 	}
-	diagForward = function(canvasInfo){
-		drawLine(canvasInfo,canvasInfo.width/4, canvasInfo.width /4)
-		
+	diagLine = function(canvasInfo, x,y){
+		drawLine(canvasInfo,canvasInfo.charWidth/4 * x, canvasInfo.charWidth/4 * y)
 	}
-	diagBack = function (canvasInfo){
-		drawLine(canvasInfo,-canvasInfo.width/4, canvasInfo.width /4)
-	}
+	
 	quartDown = function(canvasInfo){
-		drawLine(canvasInfo, 0, canvasInfo.width /4)
+		drawLine(canvasInfo, 0, canvasInfo.charWidth /4)
+	}
+	quartUp = function(canvasInfo){
+		drawLine(canvasInfo, 0, canvasInfo.charWidth/4 * -1)
 	}
 	halfCircle = function(scale, gfx){
 	var pos = {
